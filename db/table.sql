b372e5fdf18907ff21532ca4a7c8406ad3c7f1ed
IagoPorto <iagopm8@gmail.com>
2023-11-12 10:26:07 +0100

    new sql tables

--- db/table.sql
+++ db/table.sql
@@ -1,6 +1,14 @@
+CREATE TABLE USER(
+    ID SERIAL PRIMARY KEY,
+    USER_NAME VARCHAR(40),
+    PASS VARCHAR(100)
+);
+
 CREATE TABLE WhitePort (
     ID serial PRIMARY KEY,
-    type varchar(20),
-    date timestamp NOT NULL,
-    file bytea
+    FILE_TYPE varchar(20),
+    MODIFICATION_DATE timestamp NOT NULL,
+    PATH_FILE varchar(255),
+    USER_NAME VARCHAR(40),  -- Debe coincidir con el tamaño de tu_tabla_principal.user_name
+    FOREIGN KEY (USER_NAME) REFERENCES USER(USER_NAME)
 );

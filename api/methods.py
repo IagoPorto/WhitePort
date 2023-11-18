51b907bf3cc7531ec56c181c41b6ed9903437ce1
LauraBlancoPereiro <laura.blanco.pereiro@gmail.com>
2023-11-19 00:05:38 +0100

    methods delete and put

--- api/methods.py
+++ api/methods.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-from app.main import get_results,insert_whiteport_data
+from app.main import get_results,insert_whiteport_data,db_params
 
 import psycopg2
 from psycopg2 import sql
@@ -26,7 +26,7 @@ def getWhiteport():
             return jsonify(whiteport_data)
         except Exception as e:
             return str(e), 500  
-def PostWhiteport():
+def postWhiteport():
 
     if request.method == 'POST':
         try:
@@ -40,3 +40,45 @@ def PostWhiteport():
                 return response, status_code
         except Exception as e:
                 return str(e), 500  
+        
+
+def updateWhiteport(id):
+        try:
+            conn = psycopg2.connect(db_params)
+            cur = conn.cursor()
+
+            data = request.get_json()
+            type = data.get("type")
+            date = data.get("date")
+            file = data.get("file").encode('utf-8')
+
+            query = sql.SQL("UPDATE {} SET type = %s, date = %s, file = %s WHERE ID = %s").format(
+                sql.Identifier(os.environ["POSTGRES_TABLE"]))
+            cur.execute(query, (type, date, file, id))
+            conn.commit()
+
+            cur.close()
+            conn.close()
+
+            return "updated successfully", 200  
+        except Exception as e:
+            return str(e), 500  
+        
+def deleteWhiteport(id):
+        try:
+            conn = psycopg2.connect(db_params)
+            cur = conn.cursor()
+
+            query = sql.SQL("DELETE FROM {} WHERE ID = %s").format(
+                sql.Identifier(os.environ["POSTGRES_TABLE"]))
+            cur.execute(query, (id,))
+            conn.commit()
+
+            cur.close()
+            conn.close()
+
+            return "eleted successfully", 200  
+        except Exception as e:
+            return str(e), 500  
+
+       
